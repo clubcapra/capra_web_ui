32596d76b10140ece93ae6c6e9b4ee8305828862
balnian <super_f_qc@hotmail.com>
2019-05-21 14:50:15 -0400

    Rrename Footer to StatusBar

---
+++ src/components/ui/layout/StatusBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="takin-footer">footer</footer>
+</template>
+
+<script>
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class StatusBar extends Vue {}
+</script>
+
+<style lang="scss">
+.takin-footer {
+  border-top: 1px solid black;
+  height: 100%;
+  font-size: 0.75em;
+}
+</style>
