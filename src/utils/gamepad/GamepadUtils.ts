933eda6c207d8a1b57f6b5cfedd1c1f3874a9e31
balnian <super_f_qc@hotmail.com>
2019-07-05 09:12:02 +1000

    add trigger axes value to axes array

--- src/utils/gamepad/GamepadUtils.ts
+++ src/utils/gamepad/GamepadUtils.ts
@@ -1,9 +1,12 @@
+import CustomGamepad from './CustomGamepad';
+import { GamepadBtn } from './mappings/types';
+
 let joySeqId = 0
 
 export const mapGamepadToJoy = (gamepad: Gamepad) => {
   const d = new Date()
   const seconds = Math.round(d.getTime() / 1000)
-
+  const cgamepad  = new CustomGamepad(gamepad)
   return {
     header: {
       seq: joySeqId++,
@@ -13,7 +16,7 @@ export const mapGamepadToJoy = (gamepad: Gamepad) => {
       },
       frame_id: '',
     },
-    axes: gamepad.axes.map(x => x  < 0.09?0.0:x),
+    axes: gamepad.axes.map(x => x  < 0.09?0.0:x).concat([cgamepad.getButtonValue(GamepadBtn.LT),cgamepad.getButtonValue(GamepadBtn.RT)]),
     buttons: gamepad.buttons.map(x =>  ~~(x.value)) //Convert to int32 (~~)
   }
 }
