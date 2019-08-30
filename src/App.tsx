167f6ddf4bb4bb62d2c484f3382e4e04b980f832
IceSentry <c.giguere42@gmail.com>
2019-08-30 13:51:02 -0400

    remove connect on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC } from 'react'
 import { BrowserRouter, Route, Redirect } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from 'globalStyles/themes/defaultTheme'
@@ -10,32 +10,25 @@ import { GamepadManager } from 'utils/gamepad/GamepadManager'
 import { DefaultInputHandler } from 'utils/gamepad/InputHandler'
 import { Provider } from 'react-redux'
 import { store } from 'store/store'
-import { rosClient } from 'utils/ros/rosClient'
 
 const gamepadManagerInstance = new GamepadManager(new DefaultInputHandler())
 gamepadManagerInstance.start()
 
 const TeleopRedirect: FC = () => <Redirect to="/teleop" />
 
-const App: React.FC = () => {
-  useEffect(() => {
-    rosClient.connect()
-  }, [])
-
-  return (
-    <Provider store={store}>
-      <ToastContainer position={'bottom-right'} />
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <ThemeProvider theme={defaultTheme}>
-          <>
-            <GlobalStyles />
-            <Route exact path="/" component={TeleopRedirect} />
-            <Layout />
-          </>
-        </ThemeProvider>
-      </BrowserRouter>
-    </Provider>
-  )
-}
+const App: React.FC = () => (
+  <Provider store={store}>
+    <ToastContainer position={'bottom-right'} />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <ThemeProvider theme={defaultTheme}>
+        <>
+          <GlobalStyles />
+          <Route exact path="/" component={TeleopRedirect} />
+          <Layout />
+        </>
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>
+)
 
 export default App
