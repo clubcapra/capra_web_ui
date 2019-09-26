c04a558a470023b2a29b37e0816f5ee3299fbc47
IceSentry <c.giguere42@gmail.com>
2019-09-25 22:11:55 -0400

    use normal lodash to fix failing test

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from 'redux-starter-kit'
 import { rootReducer } from 'store/rootReducer'
 import { loadState, saveState } from 'store/localStorage'
-import { throttle } from 'lodash-es'
+import { throttle } from 'lodash'
 
 export const store = configureStore({
   reducer: rootReducer,
