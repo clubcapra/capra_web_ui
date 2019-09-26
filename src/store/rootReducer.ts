4d135cc4d330bbe7d53e268548f7b18149cda8ca
Charles Giguere <IceSentry@users.noreply.github.com>
2019-09-26 13:39:38 -0400

    clear localStorage and reset default state without reload (#33)
    * use normal lodash to fix failing test

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,14 +1,29 @@
-import { combineReducers } from 'redux'
+import { combineReducers, AnyAction } from 'redux'
 import { rosSlice } from 'store/modules/ros/reducer'
 import { feedSlice } from 'store/modules/feed/reducer'
 import { gamepadSlice } from 'store/modules/gamepad/reducer'
 import { pwaSlice } from 'store/modules/pwa/reducer'
+import { clearStoreCache } from 'store/localStorage'
 
-export const rootReducer = combineReducers({
+const appReducer = combineReducers({
   ros: rosSlice.reducer,
   feed: feedSlice.reducer,
   gamepad: gamepadSlice.reducer,
   pwa: pwaSlice.reducer,
 })
 
-export type GlobalState = ReturnType<typeof rootReducer>
+export const RESET_STATE = { type: 'RESET_STATE' }
+
+export const rootReducer = (
+  state: GlobalState | undefined,
+  action: AnyAction
+) => {
+  if (action.type === RESET_STATE.type) {
+    state = undefined
+    clearStoreCache()
+  }
+
+  return appReducer(state, action)
+}
+
+export type GlobalState = ReturnType<typeof appReducer>
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,17 +2,14 @@ import { configureStore } from 'redux-starter-kit'
 import { rootReducer } from 'store/rootReducer'
 import { loadState, saveState } from 'store/localStorage'
 import { throttle } from 'lodash'
-import { defaultState } from 'store/defaultState'
-
-const persistedState = loadState()
 
 export const store = configureStore({
   reducer: rootReducer,
-  preloadedState: persistedState || defaultState,
+  preloadedState: loadState(),
 })
 
 store.subscribe(
   throttle(() => {
     saveState(store.getState())
-  }, 5000)
+  }, 2000)
 )
