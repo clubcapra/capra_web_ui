3a14168bcae3c2e6b95362e41fc683b36659994d
IceSentry <c.giguere42@gmail.com>
2020-02-16 17:47:41 -0500

    Add useRefSize hook

---
+++ src/utils/hooks/useRefSize.ts
@@ -0,0 +1,15 @@
+import { useState, useLayoutEffect } from 'react'
+export function useRefSize(ref: React.RefObject<HTMLElement>) {
+  const [size, setSize] = useState([0, 0])
+  useLayoutEffect(() => {
+    function updateSize() {
+      if (ref?.current) {
+        setSize([ref.current?.clientWidth, ref.current.clientHeight])
+      }
+    }
+    window.addEventListener('resize', updateSize)
+    updateSize()
+    return () => window.removeEventListener('resize', updateSize)
+  }, [ref])
+  return size
+}
