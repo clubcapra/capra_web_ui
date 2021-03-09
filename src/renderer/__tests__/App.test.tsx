0b7d1d784a1ae341fcd2eb75fbf5b7006bdb3deb
cgiguere <c.giguere42@gmail.com>
2021-03-09 12:29:38 -0500

    Revert "remove tests that don't work with Ros"
    This reverts commit 2f0eb1e5a7ce1441ba740c1737fce194c9013ce6.

---
+++ src/renderer/__tests__/App.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../App'
+
+import { act } from 'react-dom/test-utils'
+
+it('renders without crashing ReactDOM', () => {
+  const div = document.createElement('div')
+  act(() => {
+    ReactDOM.render(<App />, div)
+  })
+  ReactDOM.unmountComponentAtNode(div)
+})
