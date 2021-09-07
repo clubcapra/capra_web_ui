fca3aa4fa8840ae75deafc53affb4287306c8a41
IceSentry <c.giguere42@gmail.com>
2021-09-06 23:31:26 -0400

    clean up Feed component

--- src/renderer/components/Feed/Feed.tsx
+++ src/renderer/components/Feed/Feed.tsx
@@ -1,20 +1,25 @@
-import React, { FC, useMemo } from 'react'
-import { useSelector } from '@/renderer/utils/hooks/typedUseSelector'
+import { Select } from '@/renderer/components/common/Select'
+import { CameraFeed } from '@/renderer/components/Feed/Feeds/CameraFeed'
+import { EmptyFeed } from '@/renderer/components/Feed/Feeds/EmptyFeed'
+import { GraphFeed } from '@/renderer/components/Feed/Feeds/GraphFeed'
+import { NoFeed } from '@/renderer/components/Feed/Feeds/NoFeed'
+import { UrdfFeed } from '@/renderer/components/Feed/Feeds/UrdfFeed'
+import { styled } from '@/renderer/globalStyles/styled'
 import {
+  feedSlice,
+  FeedType,
+  FeedTypeEnum,
   selectAllFeeds,
   selectFeedFromFeedMap,
 } from '@/renderer/store/modules/feed'
-import { FeedSelect } from '@/renderer/components/Feed/FeedSelect'
-import { FeedView } from '@/renderer/components/Feed/FeedView'
 import { useOpenClose } from '@/renderer/utils/hooks/useOpenClose'
-import { styled } from '@/renderer/globalStyles/styled'
+import React, { ChangeEvent, FC, useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-interface FeedProps {
+export const Feed: FC<{
   feed_id: string
   defaultFeed?: string
-}
-
-export const Feed: FC<FeedProps> = ({ feed_id, defaultFeed }) => {
+}> = ({ feed_id, defaultFeed }) => {
   const [isMouseOver, onOver, onLeave] = useOpenClose()
 
   const mappedFeed = useSelector(selectFeedFromFeedMap(feed_id))
@@ -43,6 +48,62 @@ export const Feed: FC<FeedProps> = ({ feed_id, defaultFeed }) => {
   )
 }
 
+const FeedView: FC<{
+  feed: FeedType
+}> = ({ feed }) => {
+  switch (feed.type) {
+    case FeedTypeEnum.Empty:
+      return <EmptyFeed />
+    case FeedTypeEnum.Camera:
+      return <CameraFeed feed={feed} />
+    case FeedTypeEnum.Urdf:
+      return <UrdfFeed feed={feed} />
+    case FeedTypeEnum.Graph:
+      return <GraphFeed feed={feed} />
+    default:
+      return <NoFeed text="NOT SUPPORTED" />
+  }
+}
+
+const getLabel = (feed: FeedType) => {
+  switch (feed.type) {
+    case FeedTypeEnum.Camera:
+      return feed.camera.name
+    case FeedTypeEnum.Graph:
+      return feed.graph.name
+    default:
+      return feed.id
+  }
+}
+
+const FeedSelect: FC<{
+  id: string
+  visible: boolean
+  currentFeedId: string
+}> = ({ id, visible, currentFeedId }) => {
+  const dispatch = useDispatch()
+  const allFeeds = useSelector(selectAllFeeds)
+  const feedCollection = Object.values(allFeeds)
+
+  const selectFeed = (e: ChangeEvent<HTMLSelectElement>) => {
+    dispatch(feedSlice.actions.updateFeedMap({ id, feedId: e.target.value }))
+  }
+
+  return (
+    <StyledSelectContainer hidden={!visible}>
+      <Select
+        onChange={selectFeed}
+        value={currentFeedId}
+        options={feedCollection.map((feed) => ({
+          key: feed.id,
+          value: feed.id,
+          content: getLabel(feed),
+        }))}
+      />
+    </StyledSelectContainer>
+  )
+}
+
 const StyledFeedComponent = styled.div`
   position: relative;
   display: flex;
@@ -51,4 +112,10 @@ const StyledFeedComponent = styled.div`
   height: 100%;
   width: 100%;
   padding: 1px;
+`
+
+const StyledSelectContainer = styled.div`
+  position: absolute;
+  bottom: 4px;
+  right: 4px;
 `
