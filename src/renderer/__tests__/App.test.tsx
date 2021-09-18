d6d529a512b889bd8f7375bea4feec503391fbda
IceSentry <c.giguere42@gmail.com>
2021-09-18 16:40:24 -0400

    clean up tests

--- src/renderer/__tests__/App.test.tsx
+++ src/renderer/__tests__/App.test.tsx
@@ -1,7 +1,20 @@
+const mockRequire = (arg: string) => {
+  switch (arg) {
+    case 'electron':
+      return {
+        ipcRenderer: {
+          on: () => {
+            return {}
+          },
+        },
+      }
+  }
+}
+window.require = mockRequire as NodeRequire
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from '../App'
-
 import { act } from 'react-dom/test-utils'
 
 it('renders without crashing ReactDOM', () => {
