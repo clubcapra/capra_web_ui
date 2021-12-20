bc95e01c207b917b7d8c17cbc1cd9e8463ab7a88
IceSentry <c.giguere42@gmail.com>
2021-12-19 21:11:41 -0500

    add validation in renderer

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,9 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+if (!window.preloadApi.app_info) {
+  // eslint-disable-next-line no-console
+  console.error('PreloadApi failed to initialize properly', window.preloadApi)
+} else {
+  ReactDOM.render(<App />, document.getElementById('root'))
 
-log.info(
-  'App finished loading... appVersion: ',
-  window.preloadApi.app_info.appVersion
-)
+  log.info(
+    'App finished loading... appVersion: ',
+    window.preloadApi.app_info.appVersion
+  )
+}
