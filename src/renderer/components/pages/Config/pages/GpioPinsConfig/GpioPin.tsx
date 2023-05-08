ec2f2f13025a523fc8761e392ff6b14da7b467d3
GLDuval <glevesqueduval@gmail.com>
2023-05-08 15:32:01 -0400

    Subscribe to topic state

--- src/renderer/components/pages/Config/pages/GpioPinsConfig/GpioPin.tsx
+++ src/renderer/components/pages/Config/pages/GpioPinsConfig/GpioPin.tsx
@@ -1,8 +1,15 @@
 import IconButton from '@/renderer/components/common/IconButton';
 import { styled } from '@/renderer/globalStyles/styled';
-import { GpioPinsState } from '@/renderer/store/modules/gpioPins';
-import React from 'react';
+import { useRosSubscribe } from '@/renderer/hooks/useRosSubscribe';
+import {
+  GpioPinsState,
+  gpioPinsSlice,
+} from '@/renderer/store/modules/gpioPins';
+import { rosClient } from '@/renderer/utils/ros/rosClient';
+import { TopicOptions } from '@/renderer/utils/ros/roslib-ts-client/@types';
+import React, { useMemo } from 'react';
 import { FaPowerOff } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
 
 interface GpioPinProps {
   gpioPin: GpioPinsState;
@@ -23,13 +30,35 @@ const Card = styled.div`
 `;
 
 export const GpioPin = ({ gpioPin }: GpioPinProps) => {
+  const dispatch = useDispatch();
+
+  const topic: TopicOptions = useMemo(
+    () => ({
+      name: gpioPin.topicName,
+      messageType: 'std_msgs/Float32',
+    }),
+    [gpioPin.topicName]
+  );
+
+  const togglePin = () => {
+    rosClient.publish(topic, gpioPin.isOn ? 0 : 1);
+
+    dispatch(gpioPinsSlice.actions.togglePin(gpioPin.id));
+  };
+
+  useRosSubscribe(topic, (message) => {
+    dispatch(
+      gpioPinsSlice.actions.updatePin({ ...gpioPin, isOn: !!message.data })
+    );
+  });
+
   return (
     <Card>
-      <h3>{gpioPin.name}</h3>
+      <h3 style={{ paddingBottom: '4px' }}>{gpioPin.name}</h3>
       <IconButton
         icon={<FaPowerOff />}
         title={gpioPin.isOn ? 'Power Off' : 'Power On'}
-        onClick={() => {}}
+        onClick={togglePin}
         style={
           gpioPin.isOn
             ? { backgroundColor: 'red' }
