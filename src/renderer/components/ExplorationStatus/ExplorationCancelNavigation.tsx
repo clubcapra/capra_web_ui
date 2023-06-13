cce13601142038540b91b38789fe03a8878dd889
amDeimos666 <71735806+amDeimos666@users.noreply.github.com>
2023-06-13 04:54:06 -0400

    add missing file

---
+++ src/renderer/components/ExplorationStatus/ExplorationCancelNavigation.tsx
@@ -0,0 +1,35 @@
+import { rosClient } from '@/renderer/utils/ros/rosClient';
+import React, { FC } from 'react';
+import { TopicOptions } from '@/renderer/utils/ros/roslib-ts-client/@types';
+import { styled } from '@/renderer/globalStyles/styled';
+import { MdOutlineCancelScheduleSend } from 'react-icons/md';
+import { toast } from 'react-toastify';
+
+const stopNavigationTopic: TopicOptions = {
+  name: '/move_base/cancel',
+  messageType: 'actionlib_msgs/GoalID',
+};
+
+interface CancelNavigation {
+  isCancelNavigationProps: () => void;
+}
+
+export const ExplorationCancelNavigation: FC<CancelNavigation> = ({
+  isCancelNavigationProps,
+}) => {
+  const onClick = () => {
+    rosClient.publish(stopNavigationTopic, { id: -1 });
+    isCancelNavigationProps();
+    toast.info('Navigation stop');
+  };
+
+  return <StyledImCancelCircle onClick={onClick} />;
+};
+
+export const StyledImCancelCircle = styled(MdOutlineCancelScheduleSend)`
+  margin: 0.25em 0.25em 0 0;
+  color: red;
+  &:hover {
+    cursor: pointer;
+  }
+`;
