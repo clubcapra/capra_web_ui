bf9396d63fe1b178dc65c27386b88fab0708c893
amDeimos666 <71735806+amDeimos666@users.noreply.github.com>
2023-06-13 04:53:41 -0400

    add stop navigation btn && fix timer display

--- src/renderer/components/ExplorationStatus/ExplorationStatus.tsx
+++ src/renderer/components/ExplorationStatus/ExplorationStatus.tsx
@@ -1,46 +1,176 @@
-import React, { FC, useState } from 'react';
 import { styled } from '@/renderer/globalStyles/styled';
-import { ExplorationTimer } from './ExplorationTimer';
+import { ExplorationCancelNavigation } from './ExplorationCancelNavigation';
 import {
   StyledPopup,
   StyledPopupContent,
   StyledPopupContainer,
 } from '@/renderer/components/styles';
 import { GoTelescope } from 'react-icons/go';
+import { Button } from '@/renderer/components/common/Button';
+import { rosClient } from '@/renderer/utils/ros/rosClient';
+import React, { useEffect, FC, useState, ChangeEvent } from 'react';
+import { LabeledInput } from '@/renderer/components/common/LabeledInput';
+import { log } from '@/renderer/logger';
 
 export const ExplorationStatus: FC = () => {
-  const [timerDisplay, setTimerDisplay] = useState('00:00');
+  const timeDisplayDefault = '00:00';
 
-  const setTimerDisplayProps = (timerToDisplay: string) => {
-    setTimerDisplay(timerToDisplay);
+  const [duration, setDuration] = useState(2);
+  const [timeRemaining, setTimeRemaining] = useState(0);
+  const [isTimerActive, setIsTimerActive] = useState(false);
+  const [timerDisplay, setTimerDisplay] = useState(timeDisplayDefault);
+  const [countDownDate, setCountDownDate] = useState(Date.now());
+
+  const updateDuration = (e: ChangeEvent<HTMLInputElement>) => {
+    setDuration(Number(e.target.value));
+  };
+
+  const startTimer = () => {
+    setIsTimerActive(false);
+    setCountDownDate(Date.now() + duration * 60 * 1000);
+    setIsTimerActive(true);
+    setRosExplorationTimer();
+  };
+
+  const stopTimer = () => {
+    setIsTimerActive(false);
+    setDuration(0);
+    setRosExplorationTimer();
   };
 
   const isShowTimerDisplay = () => {
     return timerDisplay !== '00:00';
   };
 
-  return (
-    <StyledPopup
-      trigger={
-        <StyledPopupContainer>
-          {isShowTimerDisplay() && timerDisplay}
-          <StyledGoTelescope />
-        </StyledPopupContainer>
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval> | undefined;
+    const intervalMs = 1000;
+    if (isTimerActive) {
+      interval = setInterval(() => {
+        setTimerDisplay(getTimeRemaining());
+        setTimeRemaining(timeRemaining - intervalMs);
+      }, intervalMs);
+    } else if (!isTimerActive && timeRemaining !== 0) {
+      if (interval !== undefined) {
+        clearInterval(interval);
       }
-      on="click"
-      position="bottom center"
-      arrow={false}
-      repositionOnResize={true}
-    >
-      <StyledPopupContent>
-        <ExplorationTimer setTimerDisplayProps={setTimerDisplayProps} />
-      </StyledPopupContent>
-    </StyledPopup>
+      setTimerDisplay(timeDisplayDefault);
+      setTimerDisplay(timeDisplayDefault);
+      setTimeRemaining(0);
+    }
+
+    const getTimeRemaining = () => {
+      const total = countDownDate - Date.now();
+
+      const minutes = Math.floor((total % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((total % (1000 * 60)) / 1000);
+
+      const minutesDiplay =
+        minutes < 10 ? '0' + minutes.toString() : minutes.toString();
+      const secondsDiplay =
+        seconds < 10 ? '0' + seconds.toString() : seconds.toString();
+
+      if (total < 0) {
+        setIsTimerActive(false);
+      }
+      return `${minutesDiplay}:${secondsDiplay}`;
+    };
+    return () => clearInterval(interval);
+  }, [isTimerActive, countDownDate, timeRemaining]);
+
+  const setRosExplorationTimer = () => {
+    rosClient
+      .callService(
+        {
+          name: `/start_exploration`,
+        },
+        { timeout: duration * 60 }
+      )
+      .catch(log.error);
+  };
+
+  return (
+    <StyledDiv>
+      {isShowTimerDisplay() && (
+        <ExplorationCancelNavigation isCancelNavigationProps={stopTimer} />
+      )}
+      <StyledPopup
+        trigger={
+          <StyledPopupContainer>
+            {isShowTimerDisplay() && timerDisplay}
+            <StyledGoTelescope />
+          </StyledPopupContainer>
+        }
+        on="click"
+        position="bottom center"
+        arrow={false}
+        repositionOnResize={true}
+      >
+        <StyledPopupContent>
+          <StyledDivTimer>
+            <div>
+              <LabeledInput
+                label="Duration of exploration (min)"
+                value={duration.toString()}
+                type="number"
+                onChange={updateDuration}
+              />
+              <StyledDivDuration>
+                <Button
+                  onClick={startTimer}
+                  btnType="success"
+                  style={{ maxWidth: '185px' }}
+                >
+                  Start
+                </Button>
+                <Button
+                  onClick={stopTimer}
+                  btnType="danger"
+                  style={{ maxWidth: '185px' }}
+                >
+                  Stop
+                </Button>
+              </StyledDivDuration>
+            </div>
+            <StyledDivInfo>
+              <StyledP>Time left</StyledP>
+              <p>{timerDisplay}</p>
+              {isTimerActive && (
+                <ExplorationCancelNavigation
+                  isCancelNavigationProps={stopTimer}
+                />
+              )}
+            </StyledDivInfo>
+          </StyledDivTimer>
+        </StyledPopupContent>
+      </StyledPopup>
+    </StyledDiv>
   );
 };
+
+const StyledDiv = styled.div`
+  display: flex;
+  column-gap: 5px;
+`;
 
 const StyledGoTelescope = styled(GoTelescope)`
   height: 1.25em;
   width: 1.25em;
   margin-left: 0.5em;
+`;
+const StyledDivTimer = styled.div`
+  margin: 1em;
+`;
+
+const StyledDivDuration = styled.div`
+  display: flex;
+  column-gap: 20px;
+`;
+
+const StyledP = styled.p`
+  margin-right: 20px;
+`;
+
+const StyledDivInfo = styled(StyledDivDuration)`
+  margin: 8px;
 `;
