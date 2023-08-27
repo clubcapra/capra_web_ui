103bfc58908d40486ad511b137f7d11f85cba819
Kuift Kotar <kuiftk@gmail.com>
2023-08-27 18:45:47 -0400

    added new ui element to trigger the actuator for gpio

--- src/renderer/components/pages/Config/pages/GpioPinsConfig/GpioPinsConfig.tsx
+++ src/renderer/components/pages/Config/pages/GpioPinsConfig/GpioPinsConfig.tsx
@@ -3,6 +3,7 @@ import { SectionTitle } from '../../styles';
 import { useSelector } from 'react-redux';
 import { selectAllGpioPins } from '@/renderer/store/modules/gpioPins';
 import { GpioPin } from './GpioPin';
+import { GpioBPMPin } from './GpioBPMPin';
 import { styled } from '@/renderer/globalStyles/styled';
 
 const Container = styled.div`
@@ -18,7 +19,7 @@ export const GpioPinsConfig = () => {
       <SectionTitle>GPIO Control</SectionTitle>
       <Container>
         {gpioPins.map((gpioPin) => (
-          <GpioPin key={gpioPin.id} gpioPin={gpioPin} />
+          gpioPin.bpm ? <GpioBPMPin key={gpioPin.id} gpioPin={gpioPin} /> : <GpioPin key={gpioPin.id} gpioPin={gpioPin} /> 
         ))}
       </Container>
     </>
