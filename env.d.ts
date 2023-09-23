3201b0bcc230e1a6d3bddc7d460972a568eb6532
Samuel Lachance <leuchak@slachance.ca>
2023-09-23 19:50:44 -0400

    fix vuetify color type declaration

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,9 @@
 /* eslint spaced-comment: off */
 /// <reference types="vite/client" />
 
+// Vuetify color fix.
+declare module 'vuetify/lib/util/colors.mjs';
+
 interface ImportMetaEnv {
   // see https://vitejs.dev/guide/env-and-mode.html#env-files
   // add .env variables.
