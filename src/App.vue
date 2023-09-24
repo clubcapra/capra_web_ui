2036ed84e42aff12639ae18bb6896fb259b1cbd0
Samuel Lachance <leuchak@slachance.ca>
2023-09-23 20:57:49 -0400

    Add Teleop, Victim and Config v-tabs

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { invoke } from '@tauri-apps/api/tauri';
 
-const greetMsg = ref('');
-const name = ref('');
-
-/**
- * Greet the user
- */
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke('greet', { name: name.value });
-}
+const tab = ref(null);
 </script>
 
 <template>
   <v-app>
-    <v-sheet width="300" class="mx-auto">
-      <form class="row" @submit.prevent="greet">
-        <v-form>
-          <v-container>
-            <v-text-field v-model="name" placeholder="Enter a name...." />
-          </v-container>
-        </v-form>
-        <div class="d-flex flex-column">
-          <v-btn block class="mt-4" type="submit" color="primary">Greet</v-btn>
-        </div>
-      </form>
-
-      <p>{{ greetMsg }}</p>
-    </v-sheet>
+    <v-main>
+      <v-tabs v-model="tab">
+        <v-tab value="teleop">Teleop</v-tab>
+        <v-tab value="victim">Victim</v-tab>
+        <v-tab value="config">Config</v-tab>
+      </v-tabs>
+      <v-window v-model="tab">
+        <v-window-item value="teleop">Teleop</v-window-item>
+        <v-window-item value="victim">Victim</v-window-item>
+        <v-window-item value="config">Config</v-window-item>
+      </v-window>
+    </v-main>
   </v-app>
 </template>
