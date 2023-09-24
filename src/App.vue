9b228cf0b81121018bb12193b15a13658c31b8c0
Samuel Lachance <leuchak@slachance.ca>
2023-09-23 22:44:29 -0400

    Add app bar icon

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,57 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const tab = ref(null);
+// Components
+import ConfigWindow from './components/ConfigWindow.vue';
+
+// Assets
+import logoCapra from './assets/logo.png';
+
+// Current selected tab
+const tab = ref();
 </script>
 
 <template>
-  <v-app>
-    <v-main>
+  <v-layout>
+    <v-app-bar density="compact">
       <v-tabs v-model="tab">
         <v-tab value="teleop">Teleop</v-tab>
         <v-tab value="victim">Victim</v-tab>
         <v-tab value="config">Config</v-tab>
       </v-tabs>
+      <v-spacer />
+      <v-btn icon symbol>
+        <v-icon>mdi-timer-outline</v-icon>
+      </v-btn>
+      <v-btn icon symbol>
+        <v-icon>mdi-telescope</v-icon>
+      </v-btn>
+      <v-btn icon symbol>
+        <v-icon>mdi-lightbulb-off-outline</v-icon>
+      </v-btn>
+      <v-btn icon symbol>
+        <v-icon>mdi-battery-remove-outline</v-icon>
+      </v-btn>
+      <v-btn icon symbol>
+        <v-icon>mdi-battery-remove-outline</v-icon>
+      </v-btn>
+      <v-img
+        class="mx-2"
+        :src="logoCapra"
+        max-height="25"
+        max-width="100"
+        cover
+      />
+    </v-app-bar>
+
+    <v-main>
       <v-window v-model="tab">
         <v-window-item value="teleop">Teleop</v-window-item>
         <v-window-item value="victim">Victim</v-window-item>
-        <v-window-item value="config">Config</v-window-item>
+        <v-window-item value="config">
+          <ConfigWindow />
+        </v-window-item>
       </v-window>
     </v-main>
-  </v-app>
+  </v-layout>
 </template>
